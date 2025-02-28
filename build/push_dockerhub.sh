e0bf497967647be490aac25fc39004e16fda7c5f
Lamiaa78 <100521392+Lamiaa78@users.noreply.github.com>
2025-02-28 11:17:18 +0100

    Add helm redis / rabbitmq + fixe some bug database + update le script du push

--- build/push_dockerhub.sh
+++ build/push_dockerhub.sh
@@ -1,6 +1,6 @@
-DOCKER_USERNAME="lamisb7"
-DOCKER_PASSWORD="Daka123456@"
-DOCKER_REPO=" "
+DOCKER_USERNAME=""
+DOCKER_PASSWORD=""
+DOCKER_REPO=""
 
 # Se connecter à Docker Hub
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
@@ -24,27 +24,40 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+docker build -t mssql_database:latest -f Database/Dockerfile ./
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to build mssql_database image."
+    exit 1
+fi
+
 # Taguer les images
-docker tag applicants_api:latest $DOCKER_REPO:applicants_api-latest
-docker tag identity_api:latest $DOCKER_REPO:identity_api-latest
-docker tag jobs_api:latest $DOCKER_REPO:jobs_api-latest
+docker tag applicants_api:latest $DOCKER_REPO:applicants_api
+docker tag identity_api:latest $DOCKER_REPO:identity_api
+docker tag jobs_api:latest $DOCKER_REPO:jobs_api
+docker tag mssql_database:latest $DOCKER_REPO:mssql_database
 
 # Pousser les images taguées
-docker push $DOCKER_REPO:applicants_api-latest
+docker push $DOCKER_REPO:applicants_api
 if [ $? -ne 0 ]; then
     echo "Error: Failed to push applicants_api image."
     exit 1
 fi
 
-docker push $DOCKER_REPO:identity_api-latest
+docker push $DOCKER_REPO:identity_api
 if [ $? -ne 0 ]; then
     echo "Error: Failed to push identity_api image."
     exit 1
 fi
 
-docker push $DOCKER_REPO:jobs_api-latest
+docker push $DOCKER_REPO:jobs_api
 if [ $? -ne 0 ]; then
     echo "Error: Failed to push jobs_api image."
+    exit 1
+fi
+
+docker push $DOCKER_REPO:mssql_database
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to push mssql_database image."
     exit 1
 fi
 
