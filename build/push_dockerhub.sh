68608c0c20acd9e79ee28440afab2bb54b6a2b52
Soutif Thomas <th.soutif@gmail.com>
2025-03-01 10:46:35 +0100

    renamed

--- build/push_dockerhub.sh
+++ build/push_dockerhub.sh
@@ -1,64 +1,37 @@
-DOCKER_USERNAME=""
-DOCKER_PASSWORD=""
-DOCKER_REPO=""
+#!/bin/bash
 
-# Se connecter à Docker Hub
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+#Docker credentials
+DOCKER_USERNAME="projetcloudobsm2"
 
-# Builder les images
+#Docker image names
+DOCKER_APPLICANTS="applicants_api"
+DOCKER_IDENTITY="identity_api"
+DOCKER_JOBS="jobs_api"
+DOCKER_DATABASE="postgres"
+DOCKER_WEB="web"
+
+#Log in to Docker Hub
+docker login -u "$DOCKER_USERNAME"
+
+#Build the images
 docker build -t applicants_api:latest -f Services/Applicants.Api/Dockerfile ./
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to build applicants_api image."
-    exit 1
-fi
-
 docker build -t identity_api:latest -f Services/Identity.Api/Dockerfile ./
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to build identity_api image."
-    exit 1
-fi
-
 docker build -t jobs_api:latest -f Services/Jobs.Api/Dockerfile ./
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to build jobs_api image."
-    exit 1
-fi
+docker build -t postgres:latest -f Database/Dockerfile ./
+docker build -t web:latest -f Web/Dockerfile ./
 
-docker build -t mssql_database:latest -f Database/Dockerfile ./
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to build mssql_database image."
-    exit 1
-fi
+#Tag the images
+docker tag applicants_api:latest $DOCKER_USERNAME/$DOCKER_APPLICANTS:latest
+docker tag identity_api:latest $DOCKER_USERNAME/$DOCKER_IDENTITY:latest
+docker tag jobs_api:latest $DOCKER_USERNAME/$DOCKER_JOBS:latest
+docker tag postgres:latest $DOCKER_USERNAME/$DOCKER_DATABASE:latest
+docker tag web:latest $DOCKER_USERNAME/$DOCKER_WEB:latest
 
-# Taguer les images
-docker tag applicants_api:latest $DOCKER_REPO:applicants_api
-docker tag identity_api:latest $DOCKER_REPO:identity_api
-docker tag jobs_api:latest $DOCKER_REPO:jobs_api
-docker tag mssql_database:latest $DOCKER_REPO:mssql_database
-
-# Pousser les images taguées
-docker push $DOCKER_REPO:applicants_api
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to push applicants_api image."
-    exit 1
-fi
-
-docker push $DOCKER_REPO:identity_api
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to push identity_api image."
-    exit 1
-fi
-
-docker push $DOCKER_REPO:jobs_api
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to push jobs_api image."
-    exit 1
-fi
-
-docker push $DOCKER_REPO:mssql_database
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to push mssql_database image."
-    exit 1
-fi
+#Push the tagged images
+docker push $DOCKER_USERNAME/$DOCKER_APPLICANTS:latest
+docker push $DOCKER_USERNAME/$DOCKER_IDENTITY:latest
+docker push $DOCKER_USERNAME/$DOCKER_JOBS:latest
+docker push $DOCKER_USERNAME/$DOCKER_DATABASE:latest
+docker push $DOCKER_USERNAME/$DOCKER_WEB:latest
 
 echo "All images have been successfully pushed."
