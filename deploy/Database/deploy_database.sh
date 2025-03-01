d7c9831fb890a988511ba82c005af4ac1eac738b
Soutif Thomas <th.soutif@gmail.com>
2025-03-01 17:40:41 +0100

    fix postgres pull image add credentials

--- deploy/Database/deploy_database.sh
+++ deploy/Database/deploy_database.sh
@@ -1,1 +1,2 @@
-helm install projet-sql ./helm/
+kubectl create secret docker-registry regcred --docker-server=index.docker.io --docker-username=projetcloudobsm2 --docker-password=dckr_pat_iOX5oyuSnSRTXJk2ThUcIaYnT_A
+helm upgrade --install projet-postgres ./helm/
